4cee5f1bfdce3ce57b8ba20f8d8018e71daa43bf
粽子 <76830149+1105389168@users.noreply.github.com>
2023-04-20 16:08:17 +0800

    提交打包后/dist 下的资源

--- README.md
+++
@@ -1,8 +0,0 @@
-# animating-resume
-会动的简历
-
-
-## 使用
-   * git pull        拉取代码
-   * npm install     安装依赖包
-   * npm run serve   启动服务
--- shims-vue.d.ts
+++
@@ -1,7 +0,0 @@
-declare module '*.vue' {
-    import Vue from 'vue'
-    export default Vue
-}
-
-declare module 'prismjs'
-declare module 'marked'
