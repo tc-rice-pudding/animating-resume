a2362b648613cba13515fe6ab5e5a8300d684a51
粽子 <76830149+1105389168@users.noreply.github.com>
2023-04-20 16:56:52 +0800

    去掉elementPlus

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { createApp } from 'vue';
-import ElementPlus from 'element-plus';
-import 'element-plus/dist/index.css';
+// import ElementPlus from 'element-plus';
+// import 'element-plus/dist/index.css';
 import App from './App.vue';
 
-createApp(App).use(ElementPlus).mount('#app');
+let app = createApp(App);
+// app.use(ElementPlus);
+app.mount('#app');
